7a575ded2bf160939a0f37688f787e7ab97cc831
JIYAKR <102764298+JIYAKR@users.noreply.github.com>
2022-04-04 18:20:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+    to_number=Number(content);
+    if (Number.isInteger(to_number)) {
+        
+    }
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+    canvas = createCanvas(900, 600);
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " The Apples are drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
